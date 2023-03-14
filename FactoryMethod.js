da94152c4d9769d646c5767940c42834a021a867
LiliAzatyan <109226468+LiliAzatyan@users.noreply.github.com>
2023-03-14 20:26:17 +0400

    Update FactoryMethod.js

--- FactoryMethod.js
+++ FactoryMethod.js
@@ -1,15 +1,30 @@
+//The factory method with classes
 class Fabrica {
-    constructor(name, price) {
+    constructor(product, name, price) {
+        this.product = product;
         this.name = name;
         this.price = price;
     }
 };
 class FactoryMethod { 
-    createProduct(name, price) {
-        return new Fabrica(name, price)
+    createProduct(product, name, price) {
+        return new Fabrica(product, name, price)
     }
 };
 const car = new FactoryMethod();
-console.log(car.createProduct("BMW", "$8000")); // Fabrica { name: 'name', price: 500 }
+console.log(car.createProduct("Car", "BMW", "$8000")); // Fabrica { name: 'name', price: 500 }
 const ship = new FactoryMethod();
-console.log(ship.createProduct("Ship", "$12000")); // Fabrica { name: 'Ship', price: '$12000' }
+console.log(ship.createProduct("Ship", "Angel", "$12000")); // Fabrica { name: 'Ship', price: '$12000' }
+
+//The factory method with functions
+function Fabrica (product, name, price) {
+    this.product = product;
+    this.name = name;
+    this.price = price;
+}
+function FactoryMethod() {}
+FactoryMethod.prototype.createProduct = function(product, name, price) {
+    return new Fabrica(product, name, price);
+}
+const car = new FactoryMethod();
+console.log(car.createProduct("Car", "BMW", "$80000"));
